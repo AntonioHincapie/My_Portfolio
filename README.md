c9df853863cca4aad1ecb99cd13ad5f0ad250dcd
AntonioHincapie <marcohincapiem@gmail.com>
2022-04-12 17:01:45 -0500

    Modify Readme.md and fixed code issues.
    -Added Dredwell as a Author on my project.
    -Added links for the buttons.
    -Added background to the desktop cards.

--- README.md
+++ README.md
@@ -1,15 +1,16 @@
 # My_Portfolio
+
 The first contruction of my future professional portfolio.
 
 ## Built With
 
-- HTML and CSS
+- HTML, CSS and JavaScript.
 - Boilerplate for HTML
-- Linters for HTML and CSS
+- Linters for HTML, CSS and JavaScript.
 
 ## Getting Started
 
->This repository is for setting up my first portfolio as a software developer. With this excercise I begun to build my main-page for showing my projects and my work on a friendly way.
+> This repository is for setting up my first portfolio as a software developer. With this excercise I begun to build my main-page for showing my projects and my work on a friendly way.
 
 ## Live Demo
 
@@ -22,6 +23,12 @@ The first contruction of my future professional portfolio.
 - GitHub: [@AntonioHincapie](https://github.com/AntonioHincapie)
 - Twitter: [@MarcoHincapie](https://twitter.com/MarcoHincapie)
 - LinkedIn: [LinkedIn](https://www.linkedin.com/in/marco-hincapi%C3%A9-7a76751a3/)
+
+👤 **Dredwell Velasquez**
+
+- GitHub: [@Dredwell](https://github.com/Dredwell)
+- Twitter: [@dredwell12](https://twitter.com/dredwell12)
+- LinkedIn: [LinkedIn](https://www.linkedin.com/in/dredwell-velasquez-462a60160/)
 
 ## 🤝 Contributing
 
@@ -37,7 +44,7 @@ Give a ⭐️ if you like this project!
 
 - Microverse curriculum.
 - This is Inspirated on Figma templeates.
-- My code partners and code reviewers. 
+- My code partners and code reviewers.
 
 ## 📝 License
 
--- script.js
+++ script.js
@@ -21,6 +21,7 @@ for (let i = 0; i < menuElements.length; i += 1) {
 const popupWindow = document.getElementById('popup-window');
 const buttonsProject = document.getElementsByClassName('buttonproject');
 const popupExitButton = document.getElementById('popup-exitbtn');
+const backGround = document.getElementById('popup-background');
 
 for(let i = 0; i < buttonsProject.length; i += 1) {
   buttonsProject[i].addEventListener('click', () => {
@@ -36,25 +37,24 @@ for(let i = 0; i < buttonsProject.length; i += 1) {
     const showImg = () => {
       let str = '';
       for(let j = 0; j < dataProjects[i].imgShow.length; j += 1){
-        str += `<img src="${dataProjects[i].imgShow[j]}" alt="PROJECT1">`
+        str += `<img src="${dataProjects[i].imgShow[j]}" alt="PROJECT">`
       }
       return str;
     }
-
     document.getElementById('popup-title').innerHTML = dataProjects[i].projectTitle;
     document.getElementById('languages').innerHTML = language();
     document.getElementById('main-img').src = dataProjects[i].imgShow[0];
     document.getElementById('text').innerHTML = dataProjects[i].pText;
     document.getElementById('miniatures').innerHTML = showImg();
-
+    document.getElementById('live').href = dataProjects[i].liveUrl;
+    document.getElementById('source').href = dataProjects[i].sourceUrl;
     popupWindow.style.top = `${window.scrollY}px`;
     popupWindow.style.display = 'flex';
-
-
-
+    backGround.style.display = 'block';
   });
 }
 
 popupExitButton.addEventListener('click', () => {
   popupWindow.style.display = 'none';
+  backGround.style.display = 'none';
 });
