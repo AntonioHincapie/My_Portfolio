b8fad9f7be55ec2081a81673e441ca76069c7470
Dredwell <dredwell.velasquez@gmail.com>
2022-04-12 14:36:13 -0600

    added interactions to popup window

--- projectdata.js
+++ projectdata.js
@@ -1,6 +1,6 @@
 export let dataProjects = [{
   projectTitle: 'Project name goes here',
-  popupLanguages: ['HTML/CSS', 'Ruby on Rails', 'Javascript'],
+  popupLanguages: ['HTML/CSS', 'Ruby on Rails', 'Javascript', 'React'],
   imgShow: ['./img/project1.png', './img/project1.png', './img/project1.png', './img/project1.png'],
   pText: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi.<br>Ut aliquip ex ea commodo consequat.  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi.',
   liveUrl: 'https://antoniohincapie.github.io/My_Portfolio/',
--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-import { dataProjects } from "./projectdata";
+import { dataProjects } from "./projectdata.js";
 const btnMenu = document.getElementById('btnmenu');
 const menu = document.getElementById('menu');
 const exitBtn = document.getElementById('exitbtn');
@@ -22,13 +22,39 @@ const popupWindow = document.getElementById('popup-window');
 const buttonsProject = document.getElementsByClassName('buttonproject');
 const popupExitButton = document.getElementById('popup-exitbtn');
 
+for(let i = 0; i < buttonsProject.length; i += 1) {
+  buttonsProject[i].addEventListener('click', () => {
+
+    const language = () => {
+      let str = '';
+      for(let j = 0; j < dataProjects[i].popupLanguages.length; j += 1){
+        str += `<li class="langstyle">${dataProjects[i].popupLanguages[j]}</li>`
+      }
+      return str;
+    }
+
+    const showImg = () => {
+      let str = '';
+      for(let j = 0; j < dataProjects[i].imgShow.length; j += 1){
+        str += `<img src="${dataProjects[i].imgShow[j]}" alt="PROJECT1">`
+      }
+      return str;
+    }
+
+    document.getElementById('popup-title').innerHTML = dataProjects[i].projectTitle;
+    document.getElementById('languages').innerHTML = language();
+    document.getElementById('main-img').src = dataProjects[i].imgShow[0];
+    document.getElementById('text').innerHTML = dataProjects[i].pText;
+    document.getElementById('miniatures').innerHTML = showImg();
+
+    popupWindow.style.top = `${window.scrollY}px`;
+    popupWindow.style.display = 'flex';
+
+
+
+  });
+}
+
 popupExitButton.addEventListener('click', () => {
   popupWindow.style.display = 'none';
 });
-
-for(let i = 0; i < buttonsProject.length; i += 1) {
-  buttonsProject[i].addEventListener('click', () => {
-    popupWindow.style.top = `${window.scrollY}px`;
-    popupWindow.style.display = 'flex';
-  });
-}
